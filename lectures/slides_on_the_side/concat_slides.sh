8dcf7cbd57720eaaa8aa8693861660e1562763d1
Eric C. Anderson <eric.anderson@noaa.gov>
2020-09-07 21:31:12 -0600

    Initial commit

---
+++ lectures/slides_on_the_side/concat_slides.sh
@@ -0,0 +1,29 @@
+
+
+PDFconcat --output testing.pdf ../diagrams/slide_simple1.pdf \
+	../slide_grabs/dag-notes.pdf \
+	../diagrams/slide_infer1.pdf \
+	../figures/beta_densities.pdf \
+	../diagrams/slide_infer1-with-prior.pdf \
+	../figures/pa_posterior.pdf \
+	../slide_grabs/random-walk.pdf  \
+	/Users/eriq/Documents/git-repos/con-gen-2019/images/struct.pdf \
+	../diagrams/slide_genos-and-reads-dag.pdf  \
+	/Users/eriq/Documents/git-repos/gbs-miscall-rates/outputs/wifl_big_pop_no_bounds.pdf \
+	/Users/eriq/Documents/git-repos/gbs-miscall-rates/outputs/simulated-nice-data.pdf \
+	/Users/eriq/Documents/git-repos/gbs-miscall-rates/outputs/lobster_big_pop.pdf \
+	/Users/eriq/Documents/git-repos/gbs-miscall-rates/outputs/snails_big_pop.pdf \
+	../diagrams/slide_geno-err-model.pdf \
+	/Users/eriq/Documents/git-repos/gbs-miscall-rates/talks/bay_area_pop_gen_BAPG/images/geno-err-model-with-read-depth.pdf \
+	/Users/eriq/Documents/git-repos/con-gen-2019/outputs/001/lobster-rd-plot.pdf \
+	/Users/eriq/Documents/git-repos/eca-premature-migration-2017/eric_notes/outputs/mykiss_hwe_plot.pdf \
+	/Users/eriq/Documents/git-repos/eca-premature-migration-2017/eric_notes/outputs/mykiss_eelp_afprior_plot.pdf \
+	/Users/eriq/Documents/git-repos/eca-premature-migration-2017/eric_notes/outputs/mykiss_umpm_afprior_plot.pdf \
+	/Users/eriq/Documents/git-repos/con-gen-2019/images/MF_HA9_varying_params.pdf \
+	/Users/eriq/Documents/git-repos/con-gen-2019/images/3-Mb-chunk-mykiss-WGS.pdf \
+	/Users/eriq/Documents/git-repos/con-gen-2019/images/3-Mb-chunk-mykiss-RAD-masked.pdf \
+	/Users/eriq/Documents/git-repos/chinook-wgs/outputs/305/raster-s-vs-f-moderate-zoom-1-sort-eco-line-pop-springy.pdf \
+	/Users/eriq/Documents/git-repos/con-gen-2019/outputs/001/non-imputed-plot.pdf \
+	/Users/eriq/Documents/git-repos/con-gen-2019/outputs/001/angsd-plot.pdf  \
+	/Users/eriq/Documents/git-repos/con-gen-2019/outputs/001/imputed-plot.pdf \
+	/Users/eriq/Documents/git-repos/con-gen-2019/outputs/001/imputed-rd-plot.pdf
